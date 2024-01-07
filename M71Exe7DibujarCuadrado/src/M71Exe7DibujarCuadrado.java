e2899c72b0349bb2478c33e30a37e84505fa0394
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-07 22:11:41 +0100

    upload another method creation solving problem exercise

---
+++ M71Exe7DibujarCuadrado/src/M71Exe7DibujarCuadrado.java
@@ -0,0 +1,17 @@
+
+public class M71Exe7DibujarCuadrado {
+
+	public static void main(String[] args) {
+		/*Hacer un programa en el que el usuario introduce por pantalla un carácter 
+		 * y un número. El programa imprime por pantalla un cuadrado con el tamaño 
+		 * y el carácter introducidos por el usuario/a.
+		 */
+		
+		
+		
+	}
+	
+	
+	
+	
+}	
